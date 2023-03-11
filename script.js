2f41c01e1b719d0886ab99c1d914975c9a356b53
eroset3 <116254084+evelyntrivette@users.noreply.github.com>
2023-03-11 00:28:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const hello = document.querySelector("h2");
+
+const interactive = document.querySelector("button");
+function sendMail(){
+    window.open('mailto:test@example.com');
+}
+
+interactive.addEventListener("click", hellothere);
